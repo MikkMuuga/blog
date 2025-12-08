bfef5b3d42ed6b20068951b068819eeed16cd19d
MikkMuuga <gregormikk@gmail.com>
2025-12-08 17:02:40 +0200

    Add softDelete to Post and fixes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,16 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['title', 'body'];
-    
+
     public function snippet(): Attribute {
-        return Attribute::get (function() {
+        return Attribute::get(function () {
             return explode("\n\n", $this->body)[0];
         });
     }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -21,13 +21,26 @@
                     <td>{{ $post->updated_at }}</td>
                     <td>
                         <div class="join">
-                            <a class="btn join-item btn-info">View</a>
-                            <a href="{{ route('posts.edit', $post) }}" class="btn join-item btn-warning">Edit</a>
-                            <form action="{{ route('posts.destroy', $post)}}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn join-item btn-error">Delete</button>
-                            </form>
+                            @if($post->trashed())
+                                <form action="{{ route('posts.restore', $post)}}" method="POST">
+                                    @csrf
+                                    @method('PATCH')
+                                    <button class="btn join-item btn-success">Restore</button>
+                                </form>
+                                <form action="{{ route('posts.permadestroy', $post)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn join-item btn-error">Perma Delete</button>
+                                </form>
+                            @else
+                                <a class="btn join-item btn-info">View</a>
+                                <a href="{{ route('posts.edit', $post) }}" class="btn join-item btn-warning">Edit</a>
+                                <form action="{{ route('posts.destroy', $post)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn join-item btn-error">Delete</button>
+                                </form>
+                            @endif
                         </div>
                     </td>
                 </tr>
