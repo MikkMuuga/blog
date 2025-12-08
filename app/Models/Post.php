33f74f30df0ecd6a46a325e8d5bde80ea9416497
MikkMuuga <gregormikk@gmail.com>
2025-12-08 20:17:04 +0200

    Add Tags

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -59,4 +59,7 @@ class Post extends Model
         public function comments() {
         return $this->hasMany(Comment::class);
     }
+        public function tags() {
+        return $this->belongsToMany(Tag::class);
+    }
 }
